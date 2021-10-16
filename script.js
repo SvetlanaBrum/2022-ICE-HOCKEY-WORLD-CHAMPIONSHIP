e1a59b42140044d1fdda7a9f3ba57c408bcddb93
Svetlana Brum <sabruhm@mail.ru>
2021-10-16 23:57:56 +0300

    changed all files
    added modal window to see results

--- script.js
+++ script.js
@@ -54,3 +54,169 @@ button.addEventListener("click", function () {
     }
     
 });
+
+
+// МОДАЛЬНЫЕ ОКНА ПРИ НАЖАТИИ НА флаг
+
+// canada
+$(function () {
+    $(".can").on("click", (event) => {
+        $(".modal-can").addClass("modalStyle");
+    });
+
+    $(".cross").on("click", (event) => {
+        $(".modal-can").removeClass("modalStyle");
+    })
+});
+
+// russia
+$(function () {
+    $(".rus").on("click", (event) => {
+        $(".modal-rus").addClass("modalStyle");
+    });
+
+    $(".cross").on("click", (event) => {
+        $(".modal-rus").removeClass("modalStyle");
+    })
+});
+
+// germany
+$(function () {
+    $(".ger").on("click", (event) => {
+        $(".modal-ger").addClass("modalStyle");
+    });
+
+    $(".cross").on("click", (event) => {
+        $(".modal-ger").removeClass("modalStyle");
+    })
+});
+
+// finland
+$(function () {
+    $(".fin").on("click", (event) => {
+        $(".modal-fin").addClass("modalStyle");
+    });
+
+    $(".cross").on("click", (event) => {
+        $(".modal-fin").removeClass("modalStyle");
+    })
+});
+
+// usa
+$(function () {
+    $(".usa").on("click", (event) => {
+        $(".modal-usa").addClass("modalStyle");
+    });
+
+    $(".cross").on("click", (event) => {
+        $(".modal-usa").removeClass("modalStyle");
+    })
+});
+
+// Czech Republic
+$(function () {
+    $(".cze").on("click", (event) => {
+        $(".modal-cze").addClass("modalStyle");
+    });
+
+    $(".cross").on("click", (event) => {
+        $(".modal-cze").removeClass("modalStyle");
+    })
+});
+
+// sweden
+$(function () {
+    $(".swe").on("click", (event) => {
+        $(".modal-swe").addClass("modalStyle");
+    });
+
+    $(".cross").on("click", (event) => {
+        $(".modal-swe").removeClass("modalStyle");
+    })
+});
+
+// switzerland
+$(function () {
+    $(".sui").on("click", (event) => {
+        $(".modal-sui").addClass("modalStyle");
+    });
+
+    $(".cross").on("click", (event) => {
+        $(".modal-sui").removeClass("modalStyle");
+    })
+});
+
+// slovakia
+$(function () {
+    $(".svk").on("click", (event) => {
+        $(".modal-svk").addClass("modalStyle");
+    });
+
+    $(".cross").on("click", (event) => {
+        $(".modal-svk").removeClass("modalStyle");
+    })
+});
+
+// denmark
+$(function () {
+    $(".den").on("click", (event) => {
+        $(".modal-den").addClass("modalStyle");
+    });
+
+    $(".cross").on("click", (event) => {
+        $(".modal-den").removeClass("modalStyle");
+    })
+});
+
+// kazakhstan
+$(function () {
+    $(".kaz").on("click", (event) => {
+        $(".modal-kaz").addClass("modalStyle");
+    });
+
+    $(".cross").on("click", (event) => {
+        $(".modal-kaz").removeClass("modalStyle");
+    })
+});
+
+// italy
+$(function () {
+    $(".ita").on("click", (event) => {
+        $(".modal-ita").addClass("modalStyle");
+    });
+
+    $(".cross").on("click", (event) => {
+        $(".modal-ita").removeClass("modalStyle");
+    })
+});
+
+// GBR
+$(function () {
+    $(".gbr").on("click", (event) => {
+        $(".modal-gbr").addClass("modalStyle");
+    });
+
+    $(".cross").on("click", (event) => {
+        $(".modal-gbr").removeClass("modalStyle");
+    })
+});
+
+// belarus
+$(function () {
+    $(".blr").on("click", (event) => {
+        $(".modal-blr").addClass("modalStyle");
+    });
+
+    $(".cross").on("click", (event) => {
+        $(".modal-blr").removeClass("modalStyle");
+    })
+});
+
+
+// Закрытие модального окна при клике вне его контентной области
+$('.modal').mouseup(function (e) {
+    let modalContent = $(".modal-content");
+    if (!modalContent.is(e.target) && modalContent.has(e.target).length === 0) {
+        $(this).removeClass('modalStyle');
+}
+});
